40cd711c07cddeb75b50ab199f12d38252769623
Moringa School <loisemburu01@gmail.com>
2019-12-09 04:49:19 +0300

    added bootstrap link

--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../..services/profile.service';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
