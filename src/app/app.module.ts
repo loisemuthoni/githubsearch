da9bef7e4444a0be1f782930defc173a35f30786
Moringa School <loisemburu01@gmail.com>
2019-12-09 12:21:21 +0300

    add a highlight directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './component/profile/profile.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    HighlightDirective
       
   ],
   imports: [
